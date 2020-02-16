37cf9c264969edc31b8ec4be5b53815a45ff4705
jean <rjeangonzalo@gmail.com>
2020-02-16 17:26:35 -0500

    [Refactor] querySelector

--- scraping-linkedin.js
+++ scraping-linkedin.js
@@ -12,7 +12,6 @@ require("dotenv").config();
     const password = '#password';
     const button = '.btn__primary--large';
     await page.waitForSelector(username);
-
     await page.type(username, process.env.ID);
     await page.waitForSelector(password);
     await page.type(password, process.env.PASS);
@@ -22,9 +21,9 @@ require("dotenv").config();
     await page.goto('https://www.linkedin.com/mynetwork/invite-connect/connections/', { waitUntil: 'networkidle2' });
     let data = await page.evaluate(() =>
         [...document.querySelectorAll('.mn-connection-card')].map(e => {
-            let nombre = e.querySelector('.mn-connection-card__name').textContent
-            let urlImage = e.querySelector('img').getAttribute('src')
-            let ocupacion = e.querySelector('.mn-connection-card__occupation').textContent
+            let nombre = e.querySelector('span[class="mn-connection-card__name t-16 t-black t-bold"]').innerText
+            let urlImage = e.querySelector('div[class="presence-entity presence-entity--size-5 ember-view"] > img').src
+            let ocupacion = e.querySelector('span[class="mn-connection-card__occupation t-14 t-black--light t-normal"]').innerText
 
             return {
                 nombre,
